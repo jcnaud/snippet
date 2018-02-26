64170bf033be69edf86b90b289e9600d9be4d42b
jcnaud <jeancharles.naud@gmail.com>
2018-02-26 12:28:29 +0100

    argparse modif, modif example

--- languages/python/argparse/main.py
+++ languages/python/argparse/main.py
@@ -23,28 +23,23 @@ if __name__ == "__main__":
     parser.add_argument("file1" , help="Input file argument")
     parser.add_argument("value1", help="Integer argument", type=int)
     parser.add_argument("--file", help="A file")
+    parser.add_argument("-f", "--config-file", 
+        help="load personnal configuration file (defaut: settings.yaml)",
+        metavar="CONF_FILE",
+        default="settings.yaml")
     parser.add_argument("-a", "--activate", help="Boolean flag", action="store_true")
-    parser.add_argument("-v","-vv", "-vvv","--verbose", help="Option used multiple times", action="count", default=0)
     parser.add_argument("-m", "--mode", type=int, choices=[0, 1, 2],
                     help="Select mode")
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-y", "--yes", action="store_true", help="Exclusive with no")
-    group.add_argument("-n", "--no", action="store_true", help="Exclusive with yes")
 
-    #Add_argument
-    #  (name or flags...)[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest]
-
-    group.add_argument("--action1", "--action2",
-        action="store",
-        nargs=3,
-        default="act1",
-        type=str,
-        choices=["act1", "act2", "act2"],
-        required=False,
-        help="The advanced option",
-        metavar="PARAM",
-        dest='other_var')
+    group_vq = parser.add_mutually_exclusive_group()
+    group_vq.add_argument("-v", "-vv", "--verbose", 
+        help="enable verbosity: -v = INFO, -vv = DEBUG ", 
+        action="count",
+        default=0)
+    group_vq.add_argument("-q", "--quiet", 
+        help="quiet mode", 
+        action="store_true")
 
     args = parser.parse_args()
     main(args)
